53eef623356a6a990dd5fff5f610ecdf75f220fc
Chinmay <chinmaypardeshi36@gmail.com>
2023-08-02 21:03:57 +0530

    Committing for Day_1

---
+++ FirstBash.sh
@@ -0,0 +1,31 @@
+#! /bin/sh
+
+#To disable any line of code or to add comments, # symbol is used
+
+#echo command is used to display messages or display values stored in the variables
+echo "This is Day_1 of BashBlaze-7-Days-of-Bash-Scripting-Challenge"
+
+#Variables are used to store a value
+a=10
+b=20
+
+echo $(($a+$b))
+
+#Built-in Variables used in Bash are
+
+#pwd command is used to display present working directory
+pwd
+
+#ppid command is used to display process ID
+ppid
+
+#hostname command is used to display name of the host
+hostname
+
+
+#Wildcards are used to search for a file with some name or with specific file extension
+ls *.sh
+
+
+#The below command lists all files in Directory
+ls -l  
